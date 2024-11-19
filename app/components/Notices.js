9cc2144efc1eb6c7f56b2f7ec9fd0e51483eb274
Safa Gayret <safagayret@gmail.com>
2024-11-19 18:39:27 +0300

    feat: notice sizes

--- app/components/Notices.js
+++ app/components/Notices.js
@@ -1,24 +1,34 @@
 'use client'
 import { useEffect } from 'react'
 export default function Notice({ data }) {
+  const setSize = (element, name) => {
+    const count = data.filter((item) => item.name === name).length
+    if (count >= 2 && count <= 5) element.classList.add('normal-size')
+    if (count > 5 && count <= 10) element.classList.add('medium-size')
+    if (count > 10 && count <= 15) element.classList.add('large-size')
+    if (count > 15 && count <= 20) element.classList.add('x-large-size')
+    if (count > 20 && count <= 25) element.classList.add('xx-large-size')
+    if (count > 25) element.classList.add('xxx-large-size')
+  }
+
   useEffect(() => {
-    const { clientWidth, clientHeight } = document.documentElement
+    // array'deki tekrarlayan elemanları kaldır
+    let localData = data.reduce((acc, current) => {
+      if (!acc.find((item) => item.name === current.name)) {
+        acc.push(current)
+      }
+      return acc
+    }, [])
 
-    function getRandomPosition() {
-      const x = Math.random() * (clientWidth - 100)
-      const y = Math.random() * (clientHeight - 100)
-      return { x, y }
-    }
-
-    for (let i = 0; i < data.length; i++) {
+    // size atama
+    for (let i = 0; i < localData.length; i++) {
       const a = document.createElement('a')
       a.classList.add('notice')
-      a.innerText = data[i].name.length > 30 ? data[i].name.slice(0, 30) + '...' : data[i].name
-      if (data[i].link) a.href = data[i].link
+      a.innerText =
+        localData[i].name.length > 30 ? localData[i].name.slice(0, 30) + '...' : localData[i].name
+      if (localData[i].link) a.href = localData[i].link
 
-      const { x, y } = getRandomPosition()
-      a.style.left = `${x}px`
-      a.style.top = `${y}px`
+      setSize(a, localData[i].name)
 
       // çakışma kontrolü
       const notices = document.querySelectorAll('.notice')
@@ -35,16 +45,37 @@ export default function Notice({ data }) {
         }
       })
 
-      // rastgele renk atama
-      const r = Math.floor(Math.random() * 256)
-      const g = Math.floor(Math.random() * 256)
-      const b = Math.floor(Math.random() * 256)
-      a.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 1)`
+      // sadece koyu renkler
+      function generateDarkColor() {
+        let r, g, b
+        do {
+          r = Math.floor(Math.random() * 255)
+          g = Math.floor(Math.random() * 255)
+          b = Math.floor(Math.random() * 255)
+        } while ((r + g + b) / 3 > 100) // Ortalama parlaklık düşük olana kadar döner
 
+        return `rgba(${r}, ${g}, ${b}, 1)`
+      }
+
+      a.style.backgroundColor = generateDarkColor()
       const angle = Math.floor(Math.random() * 10)
       a.style.transform = `rotate(${angle}deg)`
 
       const container = document.getElementById('container')
+
+      const { clientWidth, clientHeight } = document.documentElement
+
+      // rastgele pozisyon
+      const getRandomPosition = () => {
+        const x = Math.floor(Math.random() * (clientWidth - 100))
+        const y = Math.floor(Math.random() * (clientHeight - 100))
+        return { x, y }
+      }
+
+      const { x, y } = getRandomPosition()
+      a.style.left = `${x}px`
+      a.style.top = `${y}px`
+
       container.appendChild(a)
     }
   })
